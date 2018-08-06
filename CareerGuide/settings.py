c182208e83591d1f64d51638a3ffa2e4490a4ecc
Rishi Protim Bose <lp.rishiprotim@gmail.com>
2018-08-07 02:06:44 +0530

    Added Signup, Signup & Login models

--- CareerGuide/settings.py
+++ CareerGuide/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Login',
+    'Login.apps.LoginConfig',
 )
 
 MIDDLEWARE_CLASSES = (
--- Login/models.py
+++ Login/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Current_User(models.Model):
+    student_id = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    current_timestamp = models.DateField(default = timezone.now)
