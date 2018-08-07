1bc8633adc3df4948b1462e1c3c92044a3ab88e4
Rishi Protim Bose <lp.rishiprotim@gmail.com>
2018-08-08 03:58:45 +0530

    Static css, js files added

--- CareerGuide/urls.py
+++ CareerGuide/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'CareerGuide.views.home', name='home'),
@@ -10,3 +13,6 @@ urlpatterns = [
     url(r'^login/', include('Login.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
